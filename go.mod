4f509d4d6374cdea05b150a081d4d4a5e8ddf67a
luca <lucat1@users.noreply.github.com>
2020-02-21 19:50:42 +0100

    Bump randr version

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/lucat1/helmet
 
 go 1.13
 
-require github.com/lucat1/randr v0.0.3
+require (
+	github.com/lucat1/randr v0.0.6
+	golang.org/x/net v0.0.0-20200219183655-46282727080f // indirect
+	golang.org/x/tools v0.0.0-20200221152158-fe62aff31966 // indirect
+)
--- head.go
+++ head.go
@@ -2,14 +2,10 @@ package helmet
 
 import "github.com/lucat1/randr"
 
-// HeadProps are the props that the head
-// component can receive
-type HeadProps randr.BasicProps
-
 // Head is a custom element used to manage
 // all your header tags, similarly to `react-helmet`
 func Head(ctx randr.Context) string {
-	props := ctx.Props.(*HeadProps)
+	props := ctx.Props.(*randr.BasicProps)
 	// Check that the context has been set
 	// if not, we define it and we're done
 	if ctx.Data[HelmetContext] == nil {
